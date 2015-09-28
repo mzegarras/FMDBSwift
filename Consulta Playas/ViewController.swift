407be39411fa73c7dc8e074f03b9a54c6a24943b
manuelzegarra <mzegarra@gmail.com>
2015-09-27 19:12:13 -0500

    Initial Commit

---
+++ Consulta Playas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Consulta Playas
+//
+//  Created by Manuel Zegarra on 27/09/15.
+//  Copyright © 2015 M-Sonic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
